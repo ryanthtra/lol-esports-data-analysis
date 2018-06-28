b78f6dcf16d6b3339c5c3d200eecd47e0a2a3198
Ryan Transfiguracion <ryan_transfiguracion@outlook.com>
2018-06-28 03:24:50 -0700

    Changed name of a CSV file to a more accurate description.

---
+++ lol_pros_predictor/analysis.R
@@ -0,0 +1,6 @@
+library(dplyr)
+library(tidyr)
+
+nalcs_matches_champ_bans <- read.csv("datasets/nalcs/nalcs_spring2018_champ_bans.csv")
+nalcs_matches_player_stats <- read.csv("datasets/nalcs/nalcs_spring2018_match_player_stats.csv")
+nalcs_matches_team_stats <- read.csv("datasets/nalcs/nalcs_spring2018_match_team_stats.csv")
