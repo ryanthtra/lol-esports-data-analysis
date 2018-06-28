250eaf5d4b079d26f7d60a7c44f62a1149bfdaba
Ryan Transfiguracion <ryan_transfiguracion@outlook.com>
2018-06-28 03:33:15 -0700

    Added analysis.R file for writing future analysis code.

--- lol_pros_predictor/analysis.R
+++ lol_pros_predictor/analysis.R
@@ -1,6 +1,9 @@
 library(dplyr)
 library(tidyr)
 
+# The "template" datasets created from Script.R
 nalcs_matches_champ_bans <- read.csv("datasets/nalcs/nalcs_spring2018_champ_bans.csv")
 nalcs_matches_player_stats <- read.csv("datasets/nalcs/nalcs_spring2018_match_player_stats.csv")
 nalcs_matches_team_stats <- read.csv("datasets/nalcs/nalcs_spring2018_match_team_stats.csv")
+
+
